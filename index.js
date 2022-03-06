60dea54203d3850cc87190d1c4614fff036fc4aa
Harshit Pratap Singh <singhharshitpratap@gmail.com>
2022-03-06 19:30:25 +0530

    multi user

--- index.js
+++ index.js
@@ -23,13 +23,13 @@ io.on("connection", function (socket) {
     let room = rooms.get(roomName);
 
     if (room == undefined) {
-      socket.join(roomName);
-      console.log("room created: ", roomName);
-      socket.emit("created");
-    } else if (room.size < 3) {
-      socket.join(roomName);
-      console.log("join to room: ", roomName);
-      socket.emit("joined");
+      socket.join([roomName, socket.id]);
+      console.log("room created: ", roomName, " ", socket.id);
+      socket.emit("created", socket.id);
+    } else if (room.size < 10) {
+      socket.join([roomName, socket.id]);
+      console.log("join to room: ", roomName, " ", socket.id);
+      socket.emit("joined", socket.id);
     } else {
       console.log("room is full: ", roomName);
       socket.emit("full");
@@ -37,30 +37,30 @@ io.on("connection", function (socket) {
     console.log(rooms);
   });
 
-  socket.on("ready", function (roomName) {
+  socket.on("ready", function (roomName, id) {
     // ready is an event that triggred by client when he join to room
-    console.log("Ready");
-    socket.broadcast.to(roomName).emit("ready");
+    console.log("Ready", id);
+    socket.broadcast.to(roomName).emit("ready", id);
   });
 
-  socket.on("candidate", function (candidate, roomName) {
-    console.log("candidate: ", candidate);
+  socket.on("candidate", function (candidate, roomName, id, to) {
+    console.log("candidate: ", id, " ", to);
 
     // information about ip address of internet connection of user.
-    socket.broadcast.to(roomName).emit("candidate", candidate);
+    socket.broadcast.to(to).emit("candidate", candidate, id);
   });
 
-  socket.on("offer", function (offer, roomName) {
-    console.log("offer: ", offer);
+  socket.on("offer", function (offer, roomName, id, to) {
+    console.log("offer: ", id, " ", to);
     // information about encoding of call or is call is audio or video etc.
-    socket.broadcast.to(roomName).emit("offer", offer);
+    socket.broadcast.to(to).emit("offer", offer, id);
   });
 
-  socket.on("answer", function (answer, roomName) {
-    console.log("answer");
+  socket.on("answer", function (answer, roomName, id, to) {
+    console.log("answer", id, " ", to);
 
     // after getting an answer communication is started.
-    socket.broadcast.to(roomName).emit("answer", answer);
+    socket.broadcast.to(to).emit("answer", answer, id);
   });
 
   socket.on("leave", function (roomName) {
